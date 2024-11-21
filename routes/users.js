2d7c92c3a7bc4005929a774eaf321e37dcfdaa9a
BoberXmolodoy <138127576+BoberXmolodoy@users.noreply.github.com>
2024-11-21 13:18:16 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
-// Сторінка перегляду всіх користувачів
+
 router.get('/', async (req, res) => {
     try {
         const users = await User.find();
@@ -13,12 +13,12 @@ router.get('/', async (req, res) => {
     }
 });
 
-// Сторінка додавання нового користувача
+
 router.get('/new', (req, res) => {
     res.render('user_new');
 });
 
-// Обробка додавання нового користувача
+
 router.post('/', async (req, res) => {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -32,7 +32,7 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Сторінка редагування користувача
+
 router.get('/:id/edit', async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
@@ -42,7 +42,7 @@ router.get('/:id/edit', async (req, res) => {
     }
 });
 
-// Обробка редагування користувача
+
 router.post('/:id', async (req, res) => {
     const { username, password } = req.body;
     const updatedUser = { username };
@@ -58,7 +58,7 @@ router.post('/:id', async (req, res) => {
     }
 });
 
-// Обробка видалення користувача
+
 router.post('/:id/delete', async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id);
